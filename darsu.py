690b2e6df5a5a9e08ffa12ca7096165f3d20c540
Darshana-Bhandari <bandaridarshana@gmail.com>
2026-06-29 11:59:28 +0545

    Initial commit

---
+++ darsu.py
@@ -0,0 +1,54 @@
+#basic Python
+# Variable & data Type
+
+
+print("darshana")
+print("my age is 19")
+
+print("Darshana." "my age is 19")
+print(23)
+print(32)
+print(23+32)
+
+name = "Darshana"
+age = 23
+price = 25.99
+print(name)
+print(age)
+print(price)
+
+print("my name is: ", name)
+print("my age is: ",age)
+print("my price is: ", price)
+
+age2 = age
+print(age2)
+
+print(type(name))
+print(type(age))
+print(type(price))
+ 
+
+name1= "darsu"
+name2 = 'darsu'
+name3= '''darsu'''
+
+print(name1)
+print(name2)
+print(name3)
+
+age3= 23
+old = False
+a= None
+print(type(old))
+print(type(a))
+
+a = 3
+b= 8
+sum = a+b
+print(sum)
+
+c = 3
+d = 8
+sum = c-d
+print(sum)
